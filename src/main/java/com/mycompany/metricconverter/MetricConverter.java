acbe8842ea04eafc35dd7cb2699acb9e6d499a66
okuhlecharlieman <114597427+okuhlecharlieman@users.noreply.github.com>
2023-06-17 12:16:41 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/metricconverter/MetricConverter.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.metricconverter;
+
+/**
+ *
+ * @author Nathan.Davids
+ */
+public class MetricConverter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
